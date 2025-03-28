fabe4b0d2e2b7d20645b91ddffb400899a8c1b93
Emily Xiong <xiongemi@gmail.com>
2025-03-28 16:28:53 -0400

    not passed --info in

--- patch.js
+++ patch.js
@@ -4,3 +4,7 @@ const { readFileSync, writeFileSync } = require('fs');
 writeFileSync(require.resolve('nx/src/tasks-runner/batch/run-batch.js'), readFileSync(
     require('path').join(__dirname, 'replacement.js')
 ));
+
+writeFileSync('node_modules/@nx/gradle/src/executors/gradlew/gradlew-batch.impl.js', readFileSync(
+    require('path').join(__dirname, 'replacement-batch.js')
+));
