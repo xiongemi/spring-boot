8de4ebb73ac70849f5992029f03e6bfdad5c08f3
Emily Xiong <xiongemi@gmail.com>
2025-04-10 15:20:19 -0400

    patch fork

--- patch.js
+++ patch.js
@@ -2,11 +2,17 @@ const { readFileSync, writeFileSync } = require('fs');
 
 console.log('Patching NX...');
 
+writeFileSync(require.resolve('nx/src/tasks-runner/task-orchestrator.js'), readFileSync(
+    require('path').join(__dirname, 'replacement-task-orchestrator.js')
+));
+
+
 /*
 writeFileSync(require.resolve('nx/src/tasks-runner/forked-process-task-runner.js'), readFileSync(
     require('path').join(__dirname, 'replacement-fork.js')
 ));
 
+/*
 writeFileSync(require.resolve('nx/src/tasks-runner/batch/run-batch.js'), readFileSync(
     require('path').join(__dirname, 'replacement.js')
 ));
