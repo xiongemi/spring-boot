644ce80e2a97feb31e705012eed5d302b3b1028f
Emily Xiong <xiongemi@gmail.com>
2025-04-16 02:31:38 -0400

    see the diff

--- patch.js
+++ patch.js
@@ -1,36 +1,22 @@
-const { readFileSync, writeFileSync } = require("fs");
+const { readFileSync, writeFileSync } = require('fs');
 
-console.log("Patching NX...");
+console.log('Patching NX...');
 
-console.log(
-  require
-    .resolve("nx/src/tasks-runner/forked-process-task-runner.js")
-    .toString()
-);
 
-console.log(
-  readFileSync(
-    require.resolve("nx/src/tasks-runner/forked-process-task-runner.js")
-  ).toString()
-);
+// writeFileSync(require.resolve('nx/src/tasks-runner/forked-process-task-runner.js'), readFileSync(
+//     require('path').join(__dirname, 'replacement-fork.js')
+// ));
 
-console.log(
-  readFileSync(
-    require("path").join(__dirname, "replacement-fork.js")
-  ).toString()
-);
 
-writeFileSync(
-  require.resolve("nx/src/tasks-runner/forked-process-task-runner.js"),
-  readFileSync(require("path").join(__dirname, "replacement-fork.js"))
-);
+// writeFileSync(require.resolve('nx/src/tasks-runner/batch/run-batch.js'), readFileSync(
+//     require('path').join(__dirname, 'replacement.js')
+// ));
 
-/*
-writeFileSync(require.resolve('nx/src/tasks-runner/batch/run-batch.js'), readFileSync(
-    require('path').join(__dirname, 'replacement.js')
-));
 
-writeFileSync('node_modules/@nx/gradle/src/executors/gradlew/gradlew-batch.impl.js', readFileSync(
-    require('path').join(__dirname, 'replacement-batch.js')
-));
-*/
+// writeFileSync('node_modules/@nx/gradle/src/executors/gradlew/gradlew-batch.impl.js', readFileSync(
+//     require('path').join(__dirname, 'replacement-batch.js')
+// ));
+
+// writeFileSync('node_modules/nx/src/tasks-runner/task-orchestrator.js', readFileSync(
+//     require('path').join(__dirname, 'replace-task-orchestrator.js')
+// ));
